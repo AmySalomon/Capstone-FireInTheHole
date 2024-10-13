f56436301030bd1283d9ee762f65f8dff85f23b8
JasonMacic <101146378+JasonMacic@users.noreply.github.com>
2024-10-13 18:07:05 -0400

    Created Weapons as ScriptableObject
    New Script WeaponClass is a ScriptableObject which hold data for the weapon's capabilities

---
+++ Fire In The Hole/Assets/Scripts/Weapons/WeaponClass.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WeaponClass/Weapon")]
+[System.Serializable]
+public class WeaponClass : ScriptableObject
+{
+    public string gunType; //name of the Gun
+    public int bulletRange, ammoCount, magazineCount; //How far the bullet travels, how many bullets are in each magazine, how many magazines each gun comes with
+    public float shootDelay, reloadSpeed, bulletSpeed; //time in between shots, time it takes to reload magazine, how fast the bullet travels
+    public Sprite gunSprite; //gun visual
+
+}

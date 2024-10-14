c356eec3e9ebf35db1486cdb5fc5fd89d9475719
JasonMacic <101146378+JasonMacic@users.noreply.github.com>
2024-10-14 01:25:45 -0400

    Revert "Merge branch 'main' into MacicBranch"
    This reverts commit 7c46d3ed045412db0dff70ca265cd7954fbc8fa4, reversing
    changes made to fab6eee160744052f0cb530147b235fb1c15fce2.

---
+++ Fire In The Hole/Assets/Scripts/Weapons/WeaponClass.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WeaponClass/Weapon")]
+[System.Serializable]
+public class WeaponClass : ScriptableObject
+{
+    public string gunType; //name of the Gun
+    public int bulletRange, ammoMax, magazineCount; //How far the bullet travels, how many bullets are in each magazine, how many magazines each gun comes with
+    //time in between shots, time it takes to reload magazine, how fast the bullet travels, how hard the screen shakes when a bullet is fired
+    public float shootDelay, reloadSpeed, launchForce, screenShake;
+    public bool automatic; //whether the player can hold down the shoot button to keep firing
+    public AudioClip gunshot; //sound made when player fires
+
+    
+    public Sprite gunSprite; //gun visual
+    
+}

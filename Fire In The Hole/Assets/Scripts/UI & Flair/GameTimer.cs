e6f4388af354159f061624930bb3e314efb0ec3e
AmySalomon <127255031+AmySalomon@users.noreply.github.com>
2024-10-17 18:23:25 -0400

    Added game timer prefab, to be added to all game scenes.
    End scene currently empty.
    Timer not on any scenes yet to avoid conflicts
    Time set to 2 minutes.

---
+++ Fire In The Hole/Assets/Scripts/UI & Flair/GameTimer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class GameTimer : MonoBehaviour
+{
+    [HideInInspector] public float timer;
+    public float startingTime;
+    public string endScreenScene;
+
+    public TextMeshProUGUI timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            updateTimer(timer);
+            if (timer < 10) timerText.color = Color.red;
+        }
+
+        else
+        {
+            timer = 0;
+            SceneManager.LoadScene(endScreenScene);
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}

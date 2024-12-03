74be6903bc30fa14910c9c2cff7ebaeb6ab8baa7
MadisonR <madison.renton5@outlook.com>
2024-12-03 15:30:39 -0500

    Replaced How to Play with Options Menu (bug with level select transitioning is not fixed.)

--- Fire In The Hole/Assets/Scripts/Menu and Level Select/LevelSelectManager.cs
+++ Fire In The Hole/Assets/Scripts/Menu and Level Select/LevelSelectManager.cs
@@ -134,7 +134,7 @@ public class LevelSelectManager : MonoBehaviour
     public void HowToPlay()
     {
         Time.timeScale = 1f;
-        chosenLevel = "LevelSelect";
+        chosenLevel = "OptionsMenu";
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
         SceneManager.LoadScene(playerSetup);

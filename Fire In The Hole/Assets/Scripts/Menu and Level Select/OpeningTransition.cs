641437257146add21af5e57658ea8c484dd5e059
MadisonR <madison.renton5@outlook.com>
2025-02-04 19:50:27 -0500

    Made the splash screen faster. Added the kill sound effect.

--- Fire In The Hole/Assets/Scripts/Menu and Level Select/OpeningTransition.cs
+++ Fire In The Hole/Assets/Scripts/Menu and Level Select/OpeningTransition.cs
@@ -8,6 +8,19 @@ public class OpeningTransition : MonoBehaviour
     //this script advances through the splash screen
     private float timer = 0;
 
+    //soundPlayer
+    private AudioSource audioPlayer;
+
+    //Dies audio
+    public AudioClip playerKilled;
+    private bool dead = false;
+
+    //Start
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -19,6 +32,15 @@ public class OpeningTransition : MonoBehaviour
             SceneManager.LoadScene("MainMenu");
             Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = true;
+        }
+
+        else if (timer > 1.5)
+        {
+            if (dead == false)
+            {
+                audioPlayer.PlayOneShot(playerKilled, 1f);
+                dead = true;
+            }
         }
     }
 }

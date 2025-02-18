c21425e580cfcf52dc982103dd4e43244d115900
AmySalomon <127255031+AmySalomon@users.noreply.github.com>
2025-02-17 19:11:22 -0500

    Bouncy bullets have been coded
    To activate via powerup, use the "canBounce" bool on bullets.

--- Fire In The Hole/Assets/Scripts/Objects (Bullets, Balls)/BulletManager.cs
+++ Fire In The Hole/Assets/Scripts/Objects (Bullets, Balls)/BulletManager.cs
@@ -9,12 +9,21 @@ public class BulletManager : MonoBehaviour
     public float deletionTime = 5;
     public GameObject playerShooter;
     public GameObject wallHitImpact;
+    private Rigidbody2D myRigidbody;
+    public PhysicsMaterial2D bounceMaterial;
 
+    [HideInInspector] public bool canBounce = false;
+
+    private Vector2 lastVelocity;
     void Start()
     {
         timer = 0;
         UpdateBulletType();
-
+        myRigidbody = GetComponent<Rigidbody2D>();
+        if (canBounce)
+        {
+            myRigidbody.sharedMaterial = bounceMaterial;
+        }
     }
 
     // Update is called once per frame
@@ -27,16 +36,31 @@ public class BulletManager : MonoBehaviour
             DestroyBullet(this.gameObject);
         }
     }
+
+    private void LateUpdate()
+    {
+        lastVelocity = myRigidbody.velocity;
+    }
     //when the bullet collides with something, check the scriptableobject for what it should do
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        bulletType.BulletCollision(collision, this.gameObject, playerShooter);
 
+        if (canBounce && collision.gameObject.tag == "Wall")
+        {
+            transform.rotation = Quaternion.FromToRotation(-Vector2.up, Vector2.Reflect(lastVelocity.normalized, collision.GetContact(0).normal.normalized));
+            canBounce = false;
+        }
+
+        else
+        {
+            bulletType.BulletCollision(collision, this.gameObject, playerShooter);
+        }
         if (collision.gameObject.tag == "Wall")
         {
             var hitImpact = Instantiate(wallHitImpact, transform.position, Quaternion.identity);
             hitImpact.transform.rotation = Quaternion.FromToRotation(Vector2.right, collision.GetContact(0).normal.normalized);
         }
+
     }
 
     public void UpdateBulletType()//Get all the info of the BulletType
